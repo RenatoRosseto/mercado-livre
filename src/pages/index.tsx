b7ade5ead0f1f073da1368612a2539d472498cd5
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2024-02-01 17:51:11 -0300

    adjusting card home container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,41 +6,51 @@ export default function Home() {
   const ContainerCards = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
     gap: 16px;
     margin-top: 16px;
+    padding: 0 16px;
+    overflow-x: auto;
+    justify-content: flex-start;
   `;
 
   return (
     <div className="container">
-      <ContainerCards>
-        <HomeCard
-          key={1}
-          title="Entre na sua conta"
-          image="images/icon-card-1.png"
-          text="Aproveite ofertas para comprar tudo que quiser"
-          buttonText="Entrar na sua conta"
-          buttonLink="https://www.mercadolivre.com.br"
-        />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <ContainerCards>
+          <HomeCard
+            key={1}
+            title="Entre na sua conta"
+            image="images/icon-card-1.png"
+            text="Aproveite ofertas para comprar tudo que quiser"
+            buttonText="Entrar na sua conta"
+            buttonLink="https://www.mercadolivre.com.br"
+          />
 
-        <HomeCard
-          key={2}
-          title="Insira sua localização"
-          image="images/icon-card-2.png"
-          text="Confira os custos e prazos de entrega"
-          buttonText="Inserir localização"
-          buttonLink="https://www.mercadolivre.com.br"
-        />
+          <HomeCard
+            key={2}
+            title="Insira sua localização"
+            image="images/icon-card-2.png"
+            text="Confira os custos e prazos de entrega"
+            buttonText="Inserir localização"
+            buttonLink="https://www.mercadolivre.com.br"
+          />
 
-        <HomeCard
-          key={3}
-          title="Meios de pagamento"
-          image="images/icon-card-3.png"
-          text="Pague suas compras com rapidez e segurança"
-          buttonText="Mostrar meios"
-          buttonLink="https://www.mercadolivre.com.br"
-        />
-      </ContainerCards>
+          <HomeCard
+            key={3}
+            title="Meios de pagamento"
+            image="images/icon-card-3.png"
+            text="Pague suas compras com rapidez e segurança"
+            buttonText="Mostrar meios"
+            buttonLink="https://www.mercadolivre.com.br"
+          />
+        </ContainerCards>
+      </div>
     </div>
   );
 }
